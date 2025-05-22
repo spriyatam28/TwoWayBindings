da125fe41d75a1e13ae69856dc28440b67a44a97
Pritam <180375336+spriyatam28@users.noreply.github.com>
2025-05-22 21:27:14 +0530

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Pritam on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
